3c6a344e9f15f423ef32b942907e6b6032f15903
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-05-16 17:06:29 +0000

    small property for white space
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@42168 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/main/java/org/mmbase/util/transformers/Identifier.java
+++ src/main/java/org/mmbase/util/transformers/Identifier.java
@@ -17,6 +17,7 @@ import org.mmbase.util.logging.*;
 /**
  * Transforms strings to identifiers, replacing punctuation and whitespace with
  * underscores.
+ * Optionally, white space can be replaced by something else (e.g. a hyphen).
  *
  * @author Pierre van Rooden
  * @since MMBase-1.7
@@ -27,6 +28,21 @@ public class Identifier extends ReaderTransformer implements CharTransformer {
     private static final long serialVersionUID = 0L;
     private static Logger log = Logging.getLoggerInstance(Identifier.class);
 
+    private String whitespace = "_";
+    /**
+     * @since MMBase-1.9.4
+     */
+    public void setWhitespaceReplacer(String ws) {
+        whitespace = ws;
+    }
+
+    /**
+     * @since MMBase-1.9.4
+     */
+    public String getWhitespaceReplacer() {
+        return whitespace;
+    }
+
     public Writer transform(Reader r, Writer w) {
         try {
             log.debug("Starting identifier");
@@ -35,6 +51,8 @@ public class Identifier extends ReaderTransformer implements CharTransformer {
                 if (c == -1) break;
                 if (Character.isLetterOrDigit((char)c)) {
                     w.write((char)c);
+                } if (Character.isWhitespace((char) c))  {
+                    w.write(whitespace);
                 } else {
                     w.write('_');
                 }
