ef670bcbb026a921e06ef137f048d788dd06ce8e
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-09-15 10:46:32 +0000

    modernized
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43319 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/test/java/org/mmbase/util/transformers/SpaceReducerTest.java
+++ src/test/java/org/mmbase/util/transformers/SpaceReducerTest.java
@@ -1,37 +1,35 @@
 package org.mmbase.util.transformers;
 import org.mmbase.util.*;
 import java.util.*;
-import junit.framework.TestCase;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 /**
 
  * @author Michiel Meeuwissen
  * @version $Id$
  */
-public class SpaceReducerTest  extends TestCase {
-
-
-    public SpaceReducerTest() {
-    }
+public class SpaceReducerTest   {
 
 
     protected static SpaceReducer reducer = new SpaceReducer();
 
 
-
-    public void testBasics() {
+    @Test
+    public void basics() {
         assertEquals("a\nb", reducer.transform("a\n\nb"));
         assertEquals("  a\n  b", reducer.transform("  a\n\n  b"));
     }
 
-    public static void test(String line, boolean opened, boolean closed){
+    protected static void test(String line, boolean opened, boolean closed){
         SpaceReducer.Tag tag = new SpaceReducer.Tag("pre");
         tag.setLine(line);
         if (opened) assertTrue(tag.hasOpened());
         if (closed) assertTrue(tag.hasClosed());
     }
 
-    public void testFindPre() {
+    @Test
+    public void findPre() {
         test("bladie hallo<pre> en nog wat<pre>daarna", true, false);
         test("bladie hallo<pre> en nog wat< / pre><   pre> <p>jaja</p> <a href=\"nogwat\">jaja</a>", true, false);
         test("jaja</pre>", false, true);
@@ -39,10 +37,12 @@ public class SpaceReducerTest  extends TestCase {
         test("jaja<pre>bla <pre /></pre>filter out bodyless tags", false, false);
     }
 
-    public void testPre() {
+    @Test
+    public void pre() {
         assertEquals("a\nb<pre>\n\nc\n\nd</pre>\ne", reducer.transform("a\n\nb<pre>\n\nc\n\nd</pre>\n\ne"));
     }
-    public void testTextarea() {
+    @Test
+    public void textarea() {
         assertEquals("a\nb<textarea>\n\nc\n\nd</textarea>\ne", reducer.transform("a\n\nb<textarea>\n\nc\n\nd</textarea>\n\ne"));
     }
 
