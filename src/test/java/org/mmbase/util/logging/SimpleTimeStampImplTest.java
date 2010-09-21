07c78f4ac147bcac07a108f2871b3b68a8ee0d1b
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-09-21 14:10:23 +0000

    test case for SimpleTimeStampImpl configuration
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43427 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/test/java/org/mmbase/util/logging/SimpleTimeStampImplTest.java
@@ -0,0 +1,55 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util.logging;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.regex.*;
+import java.io.*;
+/**
+
+ */
+public class SimpleTimeStampImplTest {
+
+    private static ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+    private static PrintStream          ORIG = System.out;
+
+    @BeforeClass
+    public static void setup() {
+        System.setOut(new PrintStream(stdout));
+    }
+
+    @AfterClass
+    public static void shutdown() {
+        System.setOut(ORIG);
+        System.out.println("shut down");
+    }
+
+
+    @Test
+    public void configure() throws IOException  {
+        SimpleTimeStampImpl.configure("A:stdout,debug B:stdout,service");
+        Logger A = SimpleTimeStampImpl.getLoggerInstance("A");
+        Logger B = SimpleTimeStampImpl.getLoggerInstance("B");
+        A.debug("a1");
+        B.debug("b1");
+        B.service("b2");
+        SimpleTimeStampImpl.configure("A:stdout,debug B:stdout,debug");
+        B.debug("b3");
+        String result = new String(stdout.toByteArray());
+        Pattern p = Pattern.compile("DEBUG \\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3} a1\\n" +
+                                    "SERVICE \\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3} b2\\n" +
+                                    "DEBUG \\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3} b3\\n"
+                                    );
+        assertTrue(p + ":" + result, p.matcher(result).matches());
+
+    }
+
+}
