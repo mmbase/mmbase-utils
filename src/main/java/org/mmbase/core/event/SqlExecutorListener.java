daf241f0ac2f83e21721bd5495525e3f1b75b921
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-09-24 15:35:44 +0000

    MMB-1985
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43459 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/main/java/org/mmbase/core/event/SqlExecutorListener.java
@@ -0,0 +1,39 @@
+/*
+ * This software is OSI Certified Open Source Software.
+ * OSI Certified is a certification mark of the Open Source Initiative. The
+ * license (Mozilla version 1.0) can be read at the MMBase site. See
+ * http://www.MMBase.org/license
+ */
+package org.mmbase.core.event;
+import org.mmbase.util.logging.*;
+
+/**
+
+ *
+ * @author Michiel Meeuwissen
+ * @since MMBase-1.9.5
+ * @version $Id: WeakNodeEventListener.java 34900 2009-05-01 16:29:42Z michiel $
+ */
+public class SqlExecutorListener extends org.mmbase.util.SqlExecutor implements SystemEventListener {
+    private static final Logger LOG = Logging.getLoggerInstance(SqlExecutorListener.class);
+
+    public SqlExecutorListener() {
+        LOG.debug("Instantiated " + this + " because ", new Exception());
+    }
+
+    public void notify(SystemEvent se) {
+        if (se instanceof SystemEvent.DataSourceAvailable) {
+            SystemEvent.DataSourceAvailable av = (SystemEvent.DataSourceAvailable) se;
+            setPrefix(av.getPrefix());
+            setDataSource(av.getDataSource());
+            run();
+        } else {
+        }
+    }
+
+    @Override
+    public String toString() {
+        return SqlExecutorListener.class.getName() + "@" + hashCode() + " " + super.toString();
+    }
+
+}
