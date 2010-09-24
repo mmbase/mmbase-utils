2c3734299b44950b81795a78a6e8d343d539ca7a
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-09-24 15:33:28 +0000

    MMB-1985
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@43457 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/main/java/org/mmbase/util/SqlExecutor.java
@@ -0,0 +1,153 @@
+/*
+
+This software is OSI Certified Open Source Software.
+OSI Certified is a certification mark of the Open Source Initiative.
+
+The license (Mozilla version 1.0) can be read at the MMBase site.
+See http://www.MMBase.org/license
+
+*/
+package org.mmbase.util;
+
+import java.util.*;
+import java.sql.*;
+import javax.sql.*;
+import java.util.*;
+import java.util.regex.*;
+import org.mmbase.util.logging.*;
+
+/**
+ * Generic tool to execute some SQL
+ *
+ * @since MMBase-1.9.5
+ * @author Michiel Meeuwissen
+ * @version $Id: FullBackupDataWriter.java 34900 2009-05-01 16:29:42Z michiel $
+ */
+
+
+public class SqlExecutor implements Runnable {
+
+    private static final Logger LOG = Logging.getLoggerInstance(SqlExecutor.class);
+
+    private String query;
+    private String update;
+    private Pattern ignore = Pattern.compile("");
+
+    protected DataSource dataSource;
+    protected String prefix;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    public void setPrefix(String pref) {
+        this.prefix = pref;
+    }
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+    public String getPrefix() {
+        if (prefix == null) {
+            return "$PREFIX";
+        } else {
+            return prefix;
+        }
+    }
+
+    public void setIgnoreException(String e) {
+        ignore = Pattern.compile(e);
+    }
+
+    public void setQuery(String q) {
+        if (update != null) throw new IllegalStateException();
+        query = q;
+    }
+    public void setUpdate(String u) {
+        if (query != null) throw new IllegalStateException();
+        update = u;
+    }
+
+    protected void executeQuery(Statement stmt, String q) throws SQLException {
+        q = q.replace("$PREFIX", getPrefix());
+        LOG.info(" Executing " + q);
+        ResultSet rs = stmt.executeQuery(q);
+        StringBuilder header = new StringBuilder();
+        for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++)  {
+            if (i > 1) {
+                header.append("|");
+            }
+            header.append(rs.getMetaData().getColumnName(i));
+        }
+        LOG.info(header);
+        int seq = 0;
+        while(true) {
+            boolean valid = rs.next();
+            if (! valid) break;
+            seq ++;
+            StringBuilder line = new StringBuilder();
+            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                if (i > 1) {
+                    line.append("|");
+                }
+                line.append(rs.getString(i));
+            }
+            LOG.info(seq + ":" + line);
+        }
+    }
+    protected void executeUpdate(Statement stmt, String u) throws SQLException {
+        u = u.replace("$PREFIX", getPrefix());
+        LOG.info(" Executing update " + u);
+        int result = stmt.executeUpdate(u);
+        LOG.info("Result :" + result);
+    }
+
+
+
+    public void run() {
+        Connection con = null;
+        Statement stmt = null;
+        try {
+            DataSource ds = getDataSource();
+            con = ds.getConnection();
+            stmt = con.createStatement();
+            if (query != null) {
+                executeQuery(stmt, query);
+            } else if (update != null) {
+                executeUpdate(stmt, update);
+            } else {
+                throw new IllegalStateException("Both query and update properties are unset");
+            }
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Throwable t) {
+            if (ignore.matcher(t.getMessage()).matches()) {
+                LOG.info("Ignoring " + t.getMessage());
+            } else {
+                throw new RuntimeException(t.getMessage(), t);
+            }
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (Exception g) {}
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception g) {}
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (update != null) {
+            return update;
+        } else if (query != null) {
+            return query;
+        } else {
+            return "No query yet";
+        }
+    }
+}
+
+
