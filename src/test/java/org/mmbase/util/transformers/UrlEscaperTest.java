47f6c9057b192f45b71f351ce81724e59fe362c7
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2011-01-05 15:17:48 +0000

    test cases for UrlEscaper
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@44524 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/test/java/org/mmbase/util/transformers/UrlEscaperTest.java
@@ -0,0 +1,37 @@
+package org.mmbase.util.transformers;
+import org.mmbase.util.*;
+import java.util.*;
+import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
+import static org.junit.Assert.*;
+
+/**
+
+ * @author Michiel Meeuwissen
+ * @version $Id$
+ */
+@RunWith(Parameterized.class)
+public class UrlEscaperTest  {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() throws Exception {
+        List<Object[]> data = new ArrayList<Object[]>();
+        data.add(new Object[] {"Caf\u00e9 e\u0125o\u015dan\u011do"});
+        data.add(new Object[] {"\u043f\u0435\u0440\u0435\u0441\u0442\u0440\u043e\u0439\u043a\u0430.txt"});
+        return data;
+    }
+
+    private String testString;
+
+    public UrlEscaperTest(String ts) {
+        testString = ts;
+    }
+
+    @Test
+    public void basic() {
+        UrlEscaper u = new UrlEscaper();
+        assertEquals(testString, u.transformBack(u.transform(testString)));
+    }
+
+}
