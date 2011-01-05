d559c0069d88adfd082a37bf3dd055ae19272767
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2011-01-05 12:36:17 +0000

    added test for Asciifier
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@44510 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/test/java/org/mmbase/util/transformers/AsciifierTest.java
@@ -0,0 +1,28 @@
+package org.mmbase.util.transformers;
+import org.mmbase.util.*;
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+
+ * @author Michiel Meeuwissen
+ * @version $Id$
+ */
+public class AsciifierTest  {
+
+
+    @Test
+    public void basic() {
+        Asciifier a = new Asciifier();
+        String testString = "Caf\u00e9 e\u0125o\u015dan\u011do";
+        assertEquals("Cafe ehosango", a.transform(testString));
+
+        a.setReplacer("_");
+        String perestrojka = "\u043f\u0435\u0440\u0435\u0441\u0442\u0440\u043e\u0439\u043a\u0430?";
+        assertEquals("___________?", a.transform(perestrojka));
+        a.setCollapseMultiple(true);
+        assertEquals("_?", a.transform(perestrojka));
+    }
+
+}
