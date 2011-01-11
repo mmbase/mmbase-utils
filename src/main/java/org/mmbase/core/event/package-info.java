e049998923a4602798b1d08cd984ad0aaab359da
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2011-01-11 22:57:25 +0000

    added package info
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@44731 91430c5f-d668-0410-9e7d-dd663366dcfd

---
+++ src/main/java/org/mmbase/core/event/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * An event mechanism. It consists of {@link Event}s, {@link EventListener}s and {@link EventBroker}s. And everything is
+ * glued together with {@link EventManager}
+ *
+ * @version $Id: package-info.java 39625 2009-11-10 23:18:05Z michiel $
+ * @since   MMBase-1.8
+ * @todo    Move to org.mmbase.event
+ */
+ package org.mmbase.core.event
