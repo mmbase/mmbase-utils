aeecab713eb743dec72874aef5ac9e369585bc55
michiel <michiel@91430c5f-d668-0410-9e7d-dd663366dcfd>
2011-01-13 11:21:31 +0000

    fixed package-info
    git-svn-id: https://scm.mmbase.org/mmbase/trunk@44822 91430c5f-d668-0410-9e7d-dd663366dcfd

--- src/main/java/org/mmbase/util/logging/package-info.java
+++ src/main/java/org/mmbase/util/logging/package-info.java
@@ -1,17 +1,17 @@
-package org.mmbase.util.logging;
 /**
  * The MMBase logging infrastructure. The main classes of this
- * package are {@link Logger}, which provides the interface for logging
+ * package are {@link org.mmbase.util.logging.Logger}, which provides the interface for logging
  * implementations (because the precise implementation is
- * pluggable), and {@link Logging}, which takes care of the configuration
+ * pluggable), and {@link org.mmbase.util.logging.Logging}, which takes care of the configuration
  * and setting up of the logging system.
  * <p>
- * Another import class is {@link Level} which describes the possible
+ * Another import class is {@link org.mmbase.util.logging.Level} which describes the possible
  * logging levels currently known by MMBase.
  * <p>
- * {@link SimpleImpl} is the most basic implementation of {@link Logger} but a
- * more sophisticated one, based on log4j, can be found in a
+ * {@link org.mmbase.util.logging.SimpleImpl} is the most basic implementation of {@link org.mmbase.util.logging.Logger} but a
+ * more sophisticated one, based on log4j (@link org.mmbase.util.logging.log4j.Log4jImpl}), can be found in a
  * subpackage.
  * @version $Id$
  * @since   MMBase-1.4
  */
+package org.mmbase.util.logging;
